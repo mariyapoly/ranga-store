d79ae2adf745ffc0964ea1cbac128566110a8bb5
mariyapoly <poly.craft2141@gmail.com>
2021-09-13 03:39:51 +0600

    added rateing and some css added

--- js/app.js
+++ js/app.js
@@ -9,7 +9,6 @@ const loadProducts = () => {
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    // console.log(product)
     const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
@@ -20,8 +19,9 @@ const showProducts = (products) => {
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <p><span>rate: ${product.rating.rate}</span><span>count: ${product.rating.count}</span></p>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now">add to cart</button>
+      <button id="details-btn" >Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
